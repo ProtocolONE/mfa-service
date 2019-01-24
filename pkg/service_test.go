c978fd504ce5d43ce38731f30e3a88a38b708f74
pr0head <pr0head@gmail.com>
2019-01-24 22:18:49 +0300

    Add more tests

--- pkg/service_test.go
+++ pkg/service_test.go
@@ -8,7 +8,9 @@ import (
 	"github.com/pquerna/otp/totp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
+	"math/rand"
 	"regexp"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -32,8 +34,8 @@ func (suite *ServiceTestSuite) SetupTest() {
 
 	suite.redis = r
 	suite.service = &mfa.Service{Redis: r}
-	suite.userID = string(time.Now().Nanosecond())
-	suite.ProviderID = string(time.Now().Nanosecond())
+	suite.userID = strconv.Itoa(random(1000000, 9999999))
+	suite.ProviderID = strconv.Itoa(random(1000000, 9999999))
 }
 
 func (suite *ServiceTestSuite) TearDownTest() {
@@ -66,6 +68,24 @@ func (suite *ServiceTestSuite) TestCreateToReturnSuccessResponse() {
 	assert.Regexp(suite.T(), regexp.MustCompile("otpauth://totp/"), res.URL)
 	assert.Regexp(suite.T(), regexp.MustCompile("https://chart.googleapis.com/chart\\?cht=qr"), res.QrCodeURL)
 	assert.Equal(suite.T(), 10, len(res.RecoveryCode))
+}
+
+func (suite *ServiceTestSuite) TestCreateToReturnUserIdAsAccountName() {
+	res := &proto.MfaCreateDataResponse{}
+	req := proto.MfaCreateDataRequest{ProviderID: suite.ProviderID, AppName: "test", UserID: suite.userID}
+	err := suite.service.Create(context.TODO(), &req, res)
+
+	assert.NoError(suite.T(), err)
+	assert.Regexp(suite.T(), regexp.MustCompile(suite.userID), res.URL)
+}
+
+func (suite *ServiceTestSuite) TestCreateToReturnEmailAsAccountName() {
+	res := &proto.MfaCreateDataResponse{}
+	req := proto.MfaCreateDataRequest{ProviderID: suite.ProviderID, AppName: "test", UserID: suite.userID, Email: "emailasaccount"}
+	err := suite.service.Create(context.TODO(), &req, res)
+
+	assert.NoError(suite.T(), err)
+	assert.Regexp(suite.T(), regexp.MustCompile("emailasaccount"), res.URL)
 }
 
 func (suite *ServiceTestSuite) TestCheckToReturnErrorRequestData() {
@@ -113,6 +133,26 @@ func (suite *ServiceTestSuite) TestCheckToReturnTrueWithRecoveryKey() {
 	assert.True(suite.T(), res2.Result)
 }
 
+func (suite *ServiceTestSuite) TestCheckToReturnFalseIfRecoveryKeysEmpty() {
+	res1 := &proto.MfaCreateDataResponse{}
+	req1 := proto.MfaCreateDataRequest{ProviderID: suite.ProviderID, AppName: "test", UserID: suite.userID}
+	suite.service.Create(context.TODO(), &req1, res1)
+
+	for i := 0; i < len(res1.RecoveryCode); i++ {
+		res2 := &proto.MfaCheckDataResponse{}
+		req2 := &proto.MfaCheckDataRequest{ProviderID: suite.ProviderID, UserID: suite.userID, Code: res1.RecoveryCode[0]}
+		suite.service.Check(context.TODO(), req2, res2)
+	}
+
+	res2 := &proto.MfaCheckDataResponse{}
+	req2 := &proto.MfaCheckDataRequest{ProviderID: suite.ProviderID, UserID: suite.userID, Code: res1.RecoveryCode[0]}
+	err2 := suite.service.Check(context.TODO(), req2, res2)
+
+	assert.NoError(suite.T(), err2)
+	assert.False(suite.T(), res2.Result)
+	assert.Equal(suite.T(), mfa.ErrorCodeInvalid, res2.Error.Message)
+}
+
 func (suite *ServiceTestSuite) TestCheckToReturnFalseWithOtpKey() {
 	res := &proto.MfaCheckDataResponse{}
 	req := &proto.MfaCheckDataRequest{ProviderID: suite.ProviderID, UserID: suite.userID, Code: "123456"}
@@ -142,4 +182,9 @@ func (suite *ServiceTestSuite) deleteKeys() error {
 		suite.service.GetRecoveryStorageKey(suite.userID, suite.ProviderID),
 		suite.service.GetSecretStorageKey(suite.userID),
 	).Err()
+}
+
+func random(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
 }
